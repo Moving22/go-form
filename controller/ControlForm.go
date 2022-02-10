1f4219da9958c6244904d130ed732be60a4b0eb6
move2u <1908125219@qq.com>
2022-02-10 20:42:51 +0800

    second commit of go-form project

--- controller/ControlForm.go
+++ controller/ControlForm.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	mod "go-form/models"
 	"strconv"
@@ -12,44 +13,116 @@ func GetPageForm(c *gin.Context)  {
 	pageNum ,_ := strconv.Atoi(c.DefaultQuery("pageNumber","1"))
 	pageSize ,_ := strconv.Atoi(c.DefaultQuery("pageSize","10"))
 
-	//从mysql查询
-	forms := mod.QueryPageForm(pageNum, pageSize)
+	//获取数据库中的数据
+	forms, err := mod.QueryPageForm(pageNum, pageSize)
 
 	//响应数据
-	c.JSON(200, gin.H{
-		"code" : 200,
-		//"msg" : "GetPageForm",
-		"data" : forms,
-		"pageSize" : pageSize,
-		"pageNumber" : pageNum,
-		"totalNumber" : pageNum * pageSize,
-	})
+	if err == nil {
+		c.JSON(200, gin.H{
+			"code" : 0,
+			"data" : forms,
+			"pageSize" : pageSize,
+			"pageNumber" : pageNum,
+			"totalNumber" : len(forms),
+		})
+	} else {
+		c.JSON(500, gin.H{
+			"code" : 1,
+			"msg" : "QueryPageForm获取数据时出错",
+		})
+	}
 }
+
 
 //获得某表单
 func GetForm(c *gin.Context)  {
-	c.JSON(200, gin.H{
-		"msg":"GetForm",
-	})
+	id := c.Param("id")
+
+	forms, err := mod.QueryFormById(id)
+
+	if err != nil {
+		c.JSON(500, gin.H{
+			"code" : 1,
+			"msg" : "QueryFormById出错",
+		})
+	}else {
+		c.JSON(200, gin.H{
+			"code" : 0,
+			"data" : forms,
+		})
+	}
 }
 
-//更新表单
+
+//编辑页面的保存表单，更新
 func UpdateForm(c *gin.Context)  {
-	c.JSON(200, gin.H{
-		"msg":"UpdateForm",
-	})
+	id := c.Param("id")
+	bytes,err := c.GetRawData()		//获取request.body参数
+
+	var m map[string]interface{}
+	json.Unmarshal(bytes, &m)	//字节转json
+
+	data, _ := json.Marshal(m["formdata"])	//取出json中的数据，转为字节
+	if data != nil{
+		err = mod.UpdateFormData(id, "formdata", string(data))
+	}
+	data, _ = json.Marshal(m["rule"])
+	if data != nil{
+		err = mod.UpdateFormData(id, "rule", string(data))
+	}
+	data, _ = json.Marshal(m["name"])
+	if data != nil{
+		err = mod.UpdateFormData(id, "name", string(data))
+	}
+
+	if err != nil {
+		c.JSON(500, gin.H{
+			"code":1,
+			"msg":"UpdateForm更新出错",
+		})
+	}else {
+		c.JSON(200, gin.H{
+			"code":0,
+		})
+	}
 }
+
 
 //删除表单
 func DeleteForm(c *gin.Context)  {
-	c.JSON(200, gin.H{
-		"msg":"DeleteForm",
-	})
+	id := c.Param("id")
+
+	err := mod.DeleteForm(id)
+
+	if err != nil {
+		c.JSON(500, gin.H{
+			"code":1,
+			"msg":"DeleteForm err",
+		})
+	}else {
+		c.JSON(200, gin.H{
+			"code":0,
+		})
+	}
 }
 
-//创建表单
+
+//创建表单，添加一个默认表单，再返回该表单
 func CreateForm(c *gin.Context)  {
-	c.JSON(200, gin.H{
-		"msg":"CreateForm",
-	})
+	var m map[string]string
+	c.BindJSON(&m)			//更优的获取json参数
+
+	id,err := mod.AddDefaultForm(m["name"])
+
+	if err != nil {
+		c.JSON(500, gin.H{
+			"code" : 1,
+			"msg" : "AddDefaultForm err",
+		})
+	}else {
+		c.JSON(200, gin.H{
+			"code" : 0,
+			"id" : id,
+		})
+	}
 }
--- models/form.go
+++ models/form.go
@@ -1,45 +1,137 @@
 package models
 
 import (
+	"encoding/json"
 	"fmt"
 	"go-form/dao"
 )
 
-
-type Form struct {
+type BaseFormInfo struct {
 	Id		int	   `json:"id"`
 	Name 	string `json:"name"`
+}
+
+type RawForm struct {
+	BaseFormInfo
 	Data 	string `json:"formdata" db:"formdata"`	//db:sqlx模块中对应的数据库字段名
-	Rule 	string `json:"rule"`
+	Rule 	string `json:"rule" db:"rule"`
 }
 
-//功能测试
-func TestQuery() []Form{
-	var forms []Form
-	sql := "select * from test limit 1,5"
-	err := dao.Db.Select(&forms, sql)
-	if err != nil {
-		fmt.Printf("select err：%v\n",err)
-		return nil
-	}
-	return forms
+type Form struct {
+	BaseFormInfo
+	Data 	map[string]interface{} `json:"formdata"`	//formdata是{}
+	Rule 	[]map[string]interface{} `json:"rule"`		//rule是[]
 }
 
-//查找一页，id降序
-func QueryPageForm(num, size int) ([]Form){
-	var forms []Form
+
+//按页查找，id降序
+func QueryPageForm(num, size int) ([]Form,error){
+	var rforms []RawForm
 	sql := "select * from test order by id desc limit ?,?"
 	begin := (num-1) * size
 	end := num * size
-	err := dao.Db.Select(&forms, sql, begin, end)
+	err := dao.Db.Select(&rforms, sql, begin, end)			//Select查多个
 	if err != nil {
 		fmt.Printf("QueryPageForm err：%v\n",err)
-		return nil
+		return nil, err
 	}
-	return forms
+
+	//解析原生表单的JSON字符串
+	size = len(rforms)
+	forms := make([]Form, size)
+	for i := 0; i < size; i++ {
+		forms[i].Id = rforms[i].Id
+		forms[i].Name = rforms[i].Name
+
+		err := json.Unmarshal([]byte(rforms[i].Data), &forms[i].Data)
+		if err != nil {
+			fmt.Printf("QueryPageForm parse data err：%v\n",err)
+			return nil, err
+		}
+		err = json.Unmarshal([]byte(rforms[i].Rule), &forms[i].Rule)
+		if err != nil {
+			fmt.Printf("QueryPageForm parse rule err：%v\n",err)
+			return nil, err
+		}
+	}
+
+	return forms, nil
 }
 
-//查找一个
-func QueryForm()  {
 
+//按名称查找
+func QueryFormByName()  {
+
+}
+
+
+//按id查看详情
+func QueryFormById(id string) (*Form, error){
+	var rform RawForm
+	sql := "select * from test where id=?"
+	err := dao.Db.Get(&rform, sql, id)			//Get查一个
+	if err != nil {
+		fmt.Printf("QueryFormById err：%v\n",err)
+		return nil, err
+	}
+
+	//解析原生表单的JSON字符串
+	var form Form
+	form.Id = rform.Id
+	form.Name = rform.Name
+
+	err = json.Unmarshal([]byte(rform.Data), &form.Data)
+	if err != nil {
+		fmt.Printf("QueryPageForm parse err：%v\n",err)
+		return nil, err
+	}
+	err = json.Unmarshal([]byte(rform.Rule), &form.Rule)
+	if err != nil {
+		fmt.Printf("QueryPageForm parse err：%v\n",err)
+		return nil, err
+	}
+
+	return &form, nil
+}
+
+
+//更新col字段的数据
+func UpdateFormData(id, col, data string) error {
+	sql := fmt.Sprintf("update test set %s=? where id=?",col)
+	_, err := dao.Db.Exec(sql, data, id)
+	if err != nil {
+		fmt.Printf("UpdateFormData err: %v\n",err)
+		return err
+	}
+
+	return nil
+}
+
+
+//删除
+func DeleteForm(id string) error {
+	sql := "delete from test where id=?"
+	_,err := dao.Db.Exec(sql, id)
+	if err != nil {
+		fmt.Printf("UpdateFormData err: %v\n",err)
+		return err
+	}
+	return nil
+}
+
+
+//增加一个默认表单
+func AddDefaultForm(name string) (int64,error) {
+	sql := "insert into test(name,formdata,rule) values(?,?,?)"
+	formdata := `{"source": []}`
+	rule := "[]"
+
+	res,err := dao.Db.Exec(sql, name, formdata, rule)
+	if err != nil {
+		fmt.Printf("AddDefaultForm err: %v\n",err)
+		return -1,err
+	}
+
+	id,_ := res.LastInsertId()
+	return id,nil
 }
